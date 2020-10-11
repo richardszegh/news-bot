b4521d67768def64d97b523b24a74701d869964d
richardszegh <richard.szegh@gmail.com>
2020-10-11 16:33:43 +0200

    ✨  add CLI lifecycle

--- news_bot/__main__.py
+++ news_bot/__main__.py
@@ -1,11 +1,55 @@
+from pprint import pprint
+from PyInquirer import prompt
+
 from news_bot.bot.hwsw import HwswBot
 from news_bot.bot.hvg import HvgBot
 from news_bot.bot.index import IndexBot
 
 
 def main():
-    indexBot = IndexBot(["kisteherautó", "térkép"])
-    indexBot.parse()
+    answers = {"sites": [], "keywords": []}
+
+    answers["sites"] = prompt(
+        [
+            {
+                "type": "checkbox",
+                "message": "Milyen hiroldalakon keressek hireket neked?",
+                "name": "news_sites",
+                "choices": [{"name": "Index"}, {"name": "HVG"}, {"name": "HWSW"}],
+            }
+        ]
+    )["news_sites"]
+
+    while True:
+        keywordAnswer = prompt(
+            [
+                {
+                    "type": "input",
+                    "message": "Kulcsszo ami erdekel teged (ha nem szeretnel tobbet megadni, hagyd uresen es nyomj egy [ENTER]-t): ",
+                    "name": "keyword",
+                }
+            ]
+        )
+        if len(keywordAnswer["keyword"]) == 0:
+            break
+        else:
+            answers["keywords"].append(keywordAnswer["keyword"])
+
+    activeBots = []
+    for site in answers["sites"]:
+        if site == "Index":
+            activeBots.append(IndexBot(answers["keywords"]))
+        elif site == "HVG":
+            activeBots.append(HvgBot(answers["keywords"]))
+        elif site == "HWSW":
+            activeBots.append(HwswBot(answers["keywords"]))
+
+    results = []
+    for bot in activeBots:
+        bot.parse()
+        results.extend(bot.results)
+
+    pprint(results)
 
 
 if __name__ == "__main__":
