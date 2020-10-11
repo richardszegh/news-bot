3a23a6c96466a2daf53bc7d7b7c10d4608812ca9
richardszegh <richard.szegh@gmail.com>
2020-10-11 15:48:14 +0200

    ✨  add Index bot

--- news_bot/__main__.py
+++ news_bot/__main__.py
@@ -1,10 +1,11 @@
 from news_bot.bot.hwsw import HwswBot
 from news_bot.bot.hvg import HvgBot
+from news_bot.bot.index import IndexBot
 
 
 def main():
-    hvgBot = HvgBot(["IbM", "Samsung"])
-    hvgBot.parse()
+    indexBot = IndexBot(["kisteherautó", "térkép"])
+    indexBot.parse()
 
 
 if __name__ == "__main__":
---
+++ news_bot/bot/index.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class IndexBot:
+    def __init__(self, keywords):
+        self.markup = requests.get("https://index.hu/24ora/").text
+        self.keywords = keywords
+        self.results = []
+
+    def parse(self):
+        soup = BeautifulSoup(self.markup, "html.parser")
+        news = soup.findAll("article", {"class": "rovatajanlo"})
+        for news_item in news:
+            title = (
+                news_item.find("a", {"class": "cim"}).text.lower().strip()
+                if news_item.find("a", {"class": "cim"})
+                else ""
+            )
+            excerpt = (
+                news_item.find("div", {"class": "ajanlo"}).text.lower().strip()
+                if news_item.find("div", {"class": "ajanlo"})
+                else ""
+            )
+            for keyword in self.keywords:
+                if (keyword.lower() in title) or (keyword.lower() in excerpt):
+                    self.results.append(news_item.find("a", {"class": "cim"})["href"])
